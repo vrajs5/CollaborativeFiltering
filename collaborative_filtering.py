bbcea782af0d702926743355a857d1b0e8aef1d7
Vishnu Chevli <vrajs5@gmail.com>
2016-04-21 13:26:52 +0530

    Corrected indentation
    Earlier indentation was making return  inside loop which was not correct...

--- collaborative_filtering.py
+++ collaborative_filtering.py
@@ -16,19 +16,19 @@ def similarity_score(person1,person2):
 		if item in dataset[person2]:
 			both_viewed[item] = 1
 
-		# Conditions to check they both have an common rating items	
-		if len(both_viewed) == 0:
-			return 0
+	# Conditions to check they both have an common rating items	
+	if len(both_viewed) == 0:
+		return 0
 
-		# Finding Euclidean distance 
-		sum_of_eclidean_distance = []	
+	# Finding Euclidean distance 
+	sum_of_eclidean_distance = []	
 
-		for item in dataset[person1]:
-			if item in dataset[person2]:
-				sum_of_eclidean_distance.append(pow(dataset[person1][item] - dataset[person2][item],2))
-		sum_of_eclidean_distance = sum(sum_of_eclidean_distance)
+	for item in dataset[person1]:
+		if item in dataset[person2]:
+			sum_of_eclidean_distance.append(pow(dataset[person1][item] - dataset[person2][item],2))
+	sum_of_eclidean_distance = sum(sum_of_eclidean_distance)
 
-		return 1/(1+sqrt(sum_of_eclidean_distance))
+	return 1/(1+sqrt(sum_of_eclidean_distance))
 
 
 
